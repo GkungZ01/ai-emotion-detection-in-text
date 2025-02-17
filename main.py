08a537f4442b6618d99ed6add7b144ece76ff253
Gkung <getjung16@gmail.com>
2025-02-17 18:55:22 +0700

    Fix Cors

--- main.py
+++ main.py
@@ -9,12 +9,26 @@ import pythainlp
 import pandas as pd
 import base64
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import uvicorn
 
 # สร้าง FastAPI application instance
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # สร้างลิสต์สำหรับเก็บข้อความและอารมณ์
 texts = []
